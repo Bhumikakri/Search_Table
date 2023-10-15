0341caf334cfb00db4b7e2406c12c57cab99ae75
Bhumikakri <128302166+Bhumikakri@users.noreply.github.com>
2023-10-15 21:03:36 +0530

    search

--- script.js
+++ script.js
@@ -1,83 +1,48 @@
-let sea = document.querySelector("input");
-let items = document.querySelectorAll(".tbody tr ");
-let bodyTable = document.querySelector(".tbody")
+let colorPic = document.getElementById("color");
+let button = document.querySelector("button");
+let Audio = document.querySelector("audio");
+let text = document.querySelector("textarea");
+let show = document.querySelector(".wrrap");
 
-sea.addEventListener('input',()=>{
-    let arrayConvert = Array.from(items);
+// ---------------for color change in input text-------------------
+text.addEventListener("input",()=>{
+    text.style.backgroundColor = colorPic.value;
+})
+// -----main button-------------------------------
+button.addEventListener("click", ()=>{
+    button.classList.add("btnn")
+    Audio.play();
 
-
-     let result = arrayConvert.filter((ele) =>{
-        let arr = ele.children[0].innerText;
-        if(arr.startsWith(sea.value)){
-           
-            return ele;
-        }
-    })
-
-    bodyTable.replaceChildren(...result);
-
+    if(text.value == ""){
+        alert("Enter some text");
+    }
+    else{
+        createElementt(text.value);
+    }
+    DeletButton();
 })
 
-// console.log(sea.value);
+// ----------------------create new divs--------------
+function createElementt(notes){
+    button.classList.remove("btnn");
+    let div1 = document.createElement('div');
+    show.appendChild(div1);
 
+    div1.innerHTML = `<p>${notes}</p>
+    <button class ="remove">Delete</button>`;
+    // --------------color for store text-------------
+    div1.style.color = colorPic.value;
+    div1.classList.add("diiv1")
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let searchBar = document.querySelector('input');
-// let table = document.querySelector('table');
-// let rows = [...document.querySelectorAll('tbody tr')];
-// let tbody = document.querySelector('tbody');
-
-// function filterData(){
-//     let searchText= searchBar.value.toLowerCase();
-//     let filterList = rows.filter((i) => {
-//         return i.firstElementChild.innerText.toLowerCase().includes(searchText);
-//     });
-//     while(tbody.firstChild){
-//         tbody.removeChild(tbody.firstChild);
-//     }
-//     for(let i=0; i<filterList.length; i++){
-//         tbody.appendChild(filterList[i]);
-//     }
-// }
-
-// searchBar.addEventListener('keyup', filterData);
+// -----------for dlt the parent divs----------------
+function DeletButton(){
+    let dlt = document.querySelectorAll(".remove");
+    dlt.forEach(element => {
+        element.addEventListener("click", ()=>{
+            // console.log(e.target);
+            element.parentElement.remove();
+        }) 
+    });
+   
+}
